d5f3aee7049984dcd04da4746b8676a987463af4
Nasta <khoirunasmuhammad@gmail.com>
2026-05-04 12:32:36 +0700

    feat: components and stuff

---
+++ src/modules/home/Hero.tsx
@@ -0,0 +1,13 @@
+import { Batik } from '@/components/elements/BatikBackground'
+
+export default function Hero() {
+  return (
+    <>
+      <Batik className="batik-5" isWhite={true} />
+      <div>
+        <h1>Hai</h1>
+        <p>Selamat datang di situs resmi KMTETI!</p>
+      </div>
+    </>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
+import { bgBatik } from './src/lib/repeater'
 
 const config: Config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
@@ -12,7 +11,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [plugin(bgBatik)],
 }
 
 export default config
