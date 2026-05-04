a72d7b72a94228f87b39d8cff1ccdb8b2661847c
Yeypayeyy <muhammadfarrelalghazy@mail.ugm.ac.id>
2026-05-04 17:07:52 +0700

    feat: add tentang section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Hero from '@/modules/home/Hero'
+import TentangSection from '@/modules/home/TentangSection'
 
 export default function HomePage() {
-  return <Hero />
+  return (
+    <>
+     <Hero />
+     <TentangSection />
+    </>
+  )
 }
+
+
