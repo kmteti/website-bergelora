98c29e7483ea0f183baa68b410d184d5cca5f320
ancung <ancungaulia@gmail.com>
2026-07-21 19:52:51 +0700

    feat: implement global smooth scrolling with lenis

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { montserrat, sourceSerif4 } from '@/lib/fonts'
 import Footer from '@/components/layout/Footer'
 import './styles.css'
 import { Navbar } from '@/components/layout/navbar/Navbar'
+import LenisProvider from '@/providers/LenisProvider'
 
 import type { Metadata, Viewport } from 'next'
 import { cn } from '@/lib/utils'
@@ -31,11 +32,13 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" className={cn(montserrat.variable, sourceSerif4.variable, 'font-sans')}>
       <body className="font-sans antialiased text-neutral-1000 flex flex-col min-h-screen">
-        <main className="flex-1 flex flex-col">
-          <Navbar />
-          {children}
-          <Footer />
-        </main>
+        <LenisProvider>
+          <main className="flex-1 flex flex-col">
+            <Navbar />
+            {children}
+            <Footer />
+          </main>
+        </LenisProvider>
       </body>
     </html>
   )
