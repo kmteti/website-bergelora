693d6a2da032072a6e482840f2714d9322c7f3ec
ancung <ancungaulia@gmail.com>
2026-08-20 15:33:48 +0700

    style(footer): simplify Connect with Us description text

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -147,7 +147,7 @@ export default function Footer() {
         <div className="max-w-[626px] mt-6 pt-6 sm:mt-0 sm:pt-16 lg:absolute lg:left-8 lg:top-[148px] lg:pt-0">
           <H1 className="text-white">Connect with Us</H1>
           <B3 className="mt-4 max-w-[34ch] font-medium text-white sm:max-w-[58ch]">
-            KMTETI senantiasa terbuka untuk menjalin kolaborasi dan bertukar gagasan. Mari bersama melangkah, berinovasi, dan hadirkan dampak positif bagi lingkungan sekitar.
+            Mari bersama melangkah, berinovasi, dan hadirkan dampak positif bagi lingkungan sekitar.
           </B3>
 
           <Link
